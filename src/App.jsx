83986baae81ef052d7cdb878afe59c8a91e54598
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-09-05 23:53:51 +0100

    create component folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <div>
+      <Navbar />
     <h4>Hi, I am here to fix you!</h4>
     <input type='text' className='input' />
     </div>
